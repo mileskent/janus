295b5e7e9016a9bf651e9fca1c767a534bcd49db
Miles K <mileskent13@gmail.com>
2024-03-09 16:20:36 -0800

    added some structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#[derive(Debug)]
+struct Time {
+    hour: u8, 
+    minute: u8
+}
+
+#[derive(Debug)]
+struct Date {
+    year: u16,
+    month: u8,
+    time: Time
+}
+
+impl Date {
+    fn month_string (&self) -> &str {
+        let months: [&str; 12] = [
+            "January", "February", "March", "April",
+            "May", "June", "July", "August", "September",
+            "October", "November", "December"
+        ];
+        match months.get((self.month as usize) - 1) {
+            Some (month) => month,
+            None => "Invalid month",
+        }
+    }
+}
+
+struct Event {
+    date: Date,
+    title: String,
+    description: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let t: Time = Time {hour: 10, minute: 30};
+    println!("{:#?}", t);
+    let d: Date = Date {year: 2024, month: 7, time: t};
+    println!("{:#?}", d);
+    println!("{}", d.month_string());
 }
